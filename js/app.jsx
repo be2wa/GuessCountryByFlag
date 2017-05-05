531e03827cb8d6e745a5cc8d00d4de365d9a3c1c
Bartlomiej Kowalczyk <bart.kowalczyk@gmail.com>
2017-05-05 22:07:27 +0200

    add timer

--- js/app.jsx
+++ js/app.jsx
@@ -5,12 +5,32 @@ require('../scss/style.scss');
 document.addEventListener('DOMContentLoaded', function(){
 
 class Header extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isOver: false
+        }
+    }
+    componentDidMount(){
+        this.timeoutId = setTimeout(() => {
+            this.setState({
+                isOver: true
+            });
+        },9000);
+    }
+    componentWillUnmount(){
+        clearTimeout(this.timeoutId);
+    }
     render(){
-        return <h1>Which country's flag is this?</h1>;
+        if(this.state.isOver){
+            return <h1>GAME OVER</h1>
+        } else {
+            return <h1>Which country's flag is this?</h1>;
+        }
     }
 }
-    
-class Game extends React.Component {
+/////////////////////////////////////////////////////////////////   
+class Flag extends React.Component {
     constructor(props){
         super(props);
         this.state = {
@@ -19,11 +39,13 @@ class Game extends React.Component {
             points: 0
         }
     }
+    
     componentDidMount(){
         this.loadQuestion();
     }
+    
     loadQuestion = () => {
-        fetch('https://restcountries.eu/rest/v2/all?fields=flag;name').then(resp => {
+        fetch('https://restcountries.eu/rest/v2/region/europe?fields=flag;name').then(resp => {
             if(resp.ok)
                 return resp.json();
             else 
@@ -45,6 +67,7 @@ class Game extends React.Component {
             console.log('Error', err);
         });
     }
+    
     validate = () => {
         let input = $('input');
         let inputVal = input.val();
@@ -59,43 +82,96 @@ class Game extends React.Component {
         }
         input.val('');
     }
+    
     render(){
         return <div>
-            <img className="flag-image" src={this.state.flag} />
-            <h2>Answer: 
-                <input type="text"></input>
-                <button onClick={this.validate} type="submit">Submit</button>
-                Points: {this.state.points}
-            </h2>
-        </div>
+                <img className="flag-image" src={this.state.flag} />
+                <Footer points={this.state.points} validate={this.validate} />
+            </div>
     }
 }
+///////////////////////////////////////////////////////////
+class Timer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            time: this.props.timeLeft
+        }
+    }
 
-class Footer extends React.Component {
+    componentDidMount(){
+       this.intervalId = setInterval(()=>{
+            this.setState({
+                time: this.state.time - 1,
+            })
+       },1000)
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.intervalId);
+    }
+
     render(){
-        return ;
+        if(this.state.time < 1){
+            clearInterval(this.intervalId)
+            return <h2>0s</h2>
+        }
+        else return <h2>{this.state.time}s</h2>
     }
 }
+////////////////////////////////////////////////////////// 
+class Footer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            time: 9,
+            isOver: false
+        }
+    }
 
+    componentDidMount(){
+        this.timeoutId = setTimeout(() => {
+            this.setState({
+                isOver: true
+            });
+        },9000);
+    }
+
+    componentWillUnmount(){
+        clearTimeout(this.timeoutId);
+    }
+
+    render(){
+        if(this.state.isOver){
+            return <div>
+                        <h2>Points: {this.props.points}</h2>
+                        <Timer />
+                    </div>;
+        } else {
+            return  <div>
+                        <h2>Answer: 
+                        <input type="text"></input>
+                        <button onClick={this.props.validate} type="submit">Submit</button>
+                        Points: {this.props.points}
+                        </h2>
+                        <Timer timeLeft={this.state.time} />
+                    </div>
+        }
+    }
+}
+/////////////////////////////////////////////////////////////
 class App extends React.Component {
     render() {
         return <div>
                 <Header />
-                <Game />
-        </div>
+                <Flag />
+            </div>
     }
 }
-
+////////////////////////////////////////////////////////////
 ReactDOM.render(
     <App />,
     document.getElementById('app')
 );
-
-//function shuffle(a) {
-//    for (let i = a.length; i; i--) {
-//        let j = Math.floor(Math.random() * i);
-//        [a[i - 1], a[j]] = [a[j], a[i - 1]];
-//    }
-//}
 
 });
